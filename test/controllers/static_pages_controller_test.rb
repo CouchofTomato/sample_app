ba5c723cc13ef3b0e598f3001df82b399f6cf42e
Austin P Mason <austinpmason@gmail.com>
2016-11-24 14:29:18 +0000

    update to chapter 6

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,16 +5,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -29,7 +23,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get contact" do
+  test "shout get contact" do
     get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
